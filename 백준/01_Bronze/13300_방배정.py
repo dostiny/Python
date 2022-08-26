1d1f3cf997f93d0ec61f22bdf13956cb10c1dd40
SSAFY <dogyeom2@gmail.com>
2022-08-26 17:50:45 +0900

    0826

--- 백준/01_Bronze/13300_방배정.py
+++ 백준/01_Bronze/13300_방배정.py
@@ -1,32 +1,23 @@
 N, K = map(int, input().split())    # 학생수, 방 인원수
 
-sut = [list(map(int, input().split())) for _ in range(N)]
-g_cnt = [0] * 6
-b_cnt = [0] * 6
+girl = [0] * 6
+boy = [0] * 6
 
-for i in range(N):
-    if sut[i][0] == 0:
-        g_cnt[sut[i][1]-1] += 1
-    if sut[i][0] == 1:
-        b_cnt[sut[i][1]-1] += 1
+for _ in range(N):
+    S, H = map(int, input().split())
+    if S == 0:
+        girl[H-1] += 1
+    elif S == 1:
+        boy[H-1] += 1
 
 result = 0
-for y in g_cnt:
-    if y == 0:
-        pass
-    elif y % K == 0:
-        result += 1
-    elif y < K:
-        result += 1
-    else:
-        result += 2
-for x in b_cnt:
-    if x == 0:
-        pass
-    elif x % K == 0:
-        result += 1
-    elif x < K:
-        result += 1
-    else:
-        result += 2
+for i in range(6):
+    if girl[i] % K == 0:
+        result += girl[i] // K
+    elif girl[i] % K != 0:
+        result += girl[i] // K + 1
+    if boy[i] % K == 0:
+        result += boy[i] // K
+    elif boy[i] % K != 0:
+        result += boy[i] // K + 1
 print(result)
