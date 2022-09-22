06b139ff2615332b7052913ebd124e48ccab2c55
dostiny <dogyeom2@gmail.com>
2022-09-23 00:11:55 +0900

    0922

---
+++ 백준/03_Gold/2493_탑.py
@@ -0,0 +1,30 @@
+import sys;
+
+N = int(sys.stdin.readline())
+top = list(map(int, sys.stdin.readline().split()))
+stack = []
+answer = [0 for i in range(N)]
+
+for i in range(N):
+    while stack:
+        if stack[-1][1] > top[i]:
+            answer[i] = stack[-1][0] + 1
+            break
+        else:
+            stack.pop()
+    stack.append([i, top[i]])
+
+print(*answer)
+
+# 시간초과
+
+# N = int(sys.stdin.readline())
+# top = list(map(int, sys.stdin.readline().split()))
+# arr = [0] * N
+# max_n = 0
+# for i in range(N-1, 0, -1):
+#     for j in range(i - 1, 0, -1):
+#         if top[i] < top[j]:
+#             arr[i] = j + 1
+#             break
+# print(*arr)
