7f99bf421b7e0f067bffd0e328901250d5cb0e89
dostiny <dogyeom2@gmail.com>
2022-12-13 18:18:06 +0900

    1213

---
+++ Python/BOJ_02_Silver/18111_마인크래프트.py
@@ -0,0 +1,11 @@
+N, M, B = map(int, input().split())
+# arr = [map(int, input().split()) for _ in range(N)]
+# print(max(arr))
+# if B == 0:
+#     pass
+# else:
+#     pass
+
+arr = []
+for _ in range(N): arr += map(int, input().split())
+print(arr)
