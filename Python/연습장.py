2da73dbed62fd942c9632eb6576cc2c19d1f9c4b
dostiny <dogyeom2@gmail.com>
2023-01-02 00:32:55 +0900

    0101

--- Python/연습장.py
+++ Python/연습장.py
@@ -32,6 +32,24 @@
 # q.append((2, 3))
 # if ww in q:
 #     print(1)
-n = 5
-light = [[[] for _ in range(n)] for _ in range(n)]
-print(light)
+# n = 5
+# light = [[[] for _ in range(n)] for _ in range(n)]
+# print(light)
+
+
+arr = [1, 3, 4, 5, 6, 2, 4, 8, 1, 5, 8, 7, 3, 4, 1, 6, 3, 4, 2, 8, 1, 4, 4, 8, 7, 5, 2, 8, 8, 1, 1, 7, 6, 2, 8, 4, 4, 5, 5, 1, 4, 7, 6, 7, 4, 7, 2, 7, 5, 5, 5, 8, 3, 4, 8, 3, 2, 7, 5, 1, 2, 7, 4, 3]
+dict = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
+acnt = bcnt = 0
+
+for i in arr:
+    dict[i] += 1
+    if i <= 4:
+        acnt += 1
+    else:
+        bcnt += 1
+
+for key, value in dict.items():
+    print(f'{key}반 : {value}명')
+print(f'비전공 {acnt + dict[4]}명 vs 전공 {bcnt - dict[4]}명')
+print(f'파이썬 {acnt}명 vs 자바 {bcnt}명')
+print(f'총 {acnt + bcnt}명')
